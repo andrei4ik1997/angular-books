07c77e44f247f5441a2bb9a18819b97bbbecb0f2
Andrei Shinkarev <ashinkarev@datamola.com>
2024-04-10 01:32:57 +0300

    Auto-generated commit [ci skip]

---
+++ chunk-EW2PVRSD.js
@@ -0,0 +1,1 @@
+import"./chunk-55KE2TB7.js";var o=[{path:"",loadComponent:()=>import("./chunk-AKPF37JZ.js"),title:"Books"}],t=o;export{t as default};
---
+++ chunk-O3JXMO3L.js
@@ -0,0 +1,1 @@
+import"./chunk-55KE2TB7.js";var o=[{path:"",loadComponent:()=>import("./chunk-4ITLS4QF.js"),title:"Not Fond"}],t=o;export{t as default};
